1ea1d48a2258243823d70371f877e10b0ffe9534
Poojan Purohit <poojanpurohit4@gmail.com>
2024-08-20 08:55:10 +0530

    Added multiply feature

---
+++ ArrayExercize/Multiply.java
@@ -0,0 +1,10 @@
+package ArrayExercize;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+        System.out.println("The multiplication is " + (a * b));
+    }
+}
